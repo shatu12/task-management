6e1cbeed1812961bfb0a3e9afff0cbdcf2e2cada
Rokeya Afroz Shatu <rokeya@cashbaba.com.bd>
2025-10-28 01:01:45 +0600

    Modify task-list component: - Add html view - Add css - Add component

--- src/app/features/task-list/task-list.ts
+++ src/app/features/task-list/task-list.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Task } from '../../core/models/task/task';
+import { TaskItem } from '../task-item/task-item';
+import { TaskStatus } from '../../core/enums/task-status';
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TaskItem],
   templateUrl: './task-list.html',
-  styleUrl: './task-list.scss',
+  styleUrls: ['./task-list.scss']
 })
 export class TaskList {
+  @Input() tasks: Task[] = [];
+  @Input() status!: TaskStatus;
+  
+  @Output() taskStatusChange = new EventEmitter<{ taskId: string; newStatus: TaskStatus }>();
+  @Output() taskDelete = new EventEmitter<string>();
 
+  onStatusChange(taskId: string, newStatus: TaskStatus): void {
+    this.taskStatusChange.emit({ taskId, newStatus });
+  }
+
+  onDelete(taskId: string): void {
+    this.taskDelete.emit(taskId);
+  }
 }
