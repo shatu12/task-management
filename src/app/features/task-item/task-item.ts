36dc30fb9b8c9ae939dfc2c0212dee57bd573502
Rokeya Afroz Shatu <rokeya@cashbaba.com.bd>
2025-10-28 01:01:18 +0600

    Modify task-item component: - Add html view - Add css - Add component

--- src/app/features/task-item/task-item.ts
+++ src/app/features/task-item/task-item.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+import { Task } from '../../core/models/task/task';
+import { TaskStatus } from '../../core/enums/task-status';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatButtonModule, RouterModule],
   templateUrl: './task-item.html',
-  styleUrl: './task-item.scss',
+  styleUrls: ['./task-item.scss']
 })
 export class TaskItem {
+  @Input() task!: Task;
+  @Output() statusChange = new EventEmitter<TaskStatus>();
+  @Output() delete = new EventEmitter<void>();
 
+  get priorityClass(): string {
+    return `priority-${this.task.priority}`;
+  }
+
+  onStatusChange(newStatus: TaskStatus): void {
+    this.statusChange.emit(newStatus);
+  }
+
+  onDelete(): void {
+    this.delete.emit();
+  }
 }
