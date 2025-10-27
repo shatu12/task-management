018800bae639ddb3665cc56cfbf522add22f2914
Rokeya Afroz Shatu <rokeya@cashbaba.com.bd>
2025-10-27 22:17:14 +0600

    Add routes: - Dash board as home page - Board - Task details - Redirect path if get any error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'board', pathMatch: 'full' },
+    { path: 'board', loadComponent: () => import('./features/board/board').then(m => m.Board) },
+    { path: 'tasks/:id', loadComponent: () => import('./features/task-details/task-details').then(m => m.TaskDetails) },
+    { path: '**', redirectTo: 'board' }
+];
