a8072a4f98f01976e822a4d1db9aa4b0f94c3a8e
Rokeya Afroz Shatu <rokeya@cashbaba.com.bd>
2025-11-09 20:52:55 +0600

    Fix bug for drag and drop

--- src/app/features/task-list/task-list.ts
+++ src/app/features/task-list/task-list.ts
@@ -1,21 +1,25 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { Task } from '../../core/models/task/task';
 import { TaskItem } from '../task-item/task-item';
 import { TaskStatus } from '../../core/enums/task-status';
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, TaskItem],
+  imports: [CommonModule, TaskItem, DragDropModule],
   templateUrl: './task-list.html',
   styleUrls: ['./task-list.scss']
 })
 export class TaskList {
   @Input() tasks: Task[] = [];
   @Input() status!: TaskStatus;
+  @Input() connectedLists: string[] = [];
   
   @Output() taskStatusChange = new EventEmitter<{ taskId: number; newStatus: TaskStatus }>();
   @Output() taskDelete = new EventEmitter<number>();
+  @Output() taskDrop = new EventEmitter<{ previousIndex: number; currentIndex: number; previousStatus: TaskStatus; newStatus: TaskStatus }>();
 
   onStatusChange(taskId: number, newStatus: TaskStatus): void {
     this.taskStatusChange.emit({ taskId, newStatus });
@@ -23,5 +27,14 @@ export class TaskList {
 
   onDelete(taskId: number): void {
     this.taskDelete.emit(taskId);
+  }
+
+  onDrop(event: { previousIndex: number; currentIndex: number; previousContainer: any; container: any }): void {
+     this.taskDrop.emit({
+      previousIndex: event.previousIndex,
+      currentIndex: event.currentIndex,
+      previousStatus: event.previousContainer.data.status,
+      newStatus: this.status
+    });
   }
 }
